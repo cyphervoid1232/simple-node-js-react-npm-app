b8aa789e3081721374609661a889b922be2385ec
Tharit Wichiankoo <tharit_8@hotmail.com>
2019-01-25 23:01:47 +0700

    fix test.bat

--- client/jenkins/scripts/test.bat
+++ client/jenkins/scripts/test.bat
@@ -13,4 +13,4 @@ cd %cd%/client
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
-
+npm test
